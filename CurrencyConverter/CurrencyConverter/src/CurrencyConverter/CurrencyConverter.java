57bf19fa74d5d4265130a898defdc45f7ecc6f0b
AjayJire790 <100845795+AjayJire790@users.noreply.github.com>
2024-01-06 09:49:11 +0530

    Add files via upload

---
+++ CurrencyConverter/CurrencyConverter/src/CurrencyConverter/CurrencyConverter.java
@@ -0,0 +1,38 @@
+package CurrencyConverter;
+
+//import com.sun.net.httpserver.Request;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.Request;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class CurrencyConverter {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type convert to convert from");
+        String convertFrom = in.nextLine();
+        System.out.println("Type currency to convert to");
+        String convertTo = in.nextLine();
+        System.out.println("Type quality to convert");
+        BigDecimal quantity = in.nextBigDecimal();
+
+        String urlString = "https://api.exchangerate.host/latest?base=" + convertFrom.toUpperCase();
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(urlString)
+                .get()
+                .build();
+        Response response = client.newCall(request).execute();
+        String stringResponse = response.body().string();
+        JSONObject jsonObject = new JSONObject(stringResponse);
+        JSONObject ratesObject = jsonObject.getJSONObject("rates");
+        BigDecimal rate = ratesObject.getBigDecimal(convertTo.toUpperCase());
+
+        BigDecimal result = rate.multiply(quantity);
+        System.out.println(result);
+    }
+}
